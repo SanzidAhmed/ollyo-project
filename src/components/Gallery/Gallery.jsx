13fe5ab38645f922ac148dc469a5d2114eb72555
SanzidAhmed <sanzid.official@gmail.com>
2023-11-05 20:43:02 +0600

    drug and drop section done

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -2,55 +2,79 @@ import React, { useEffect, useState } from 'react';
 import './Gallery.css';
 
 const Gallery = () => {
-    const [data, setData] = useState([]);
-    const [checkedItems, setCheckedItems] = useState([]);
+  const [data, setData] = useState([]);
+  const [checkedItems, setCheckedItems] = useState([]);
+  const [draggedIndex, setDraggedIndex] = useState(null);
 
-    const handleCheckboxChange = (index) => {
-        const newCheckedItems = [...checkedItems];
-        newCheckedItems[index] = !newCheckedItems[index];
-        setCheckedItems(newCheckedItems);
-    };
+  const handleCheckboxChange = (index) => {
+    const newCheckedItems = [...checkedItems];
+    newCheckedItems[index] = !newCheckedItems[index];
+    setCheckedItems(newCheckedItems);
+  };
 
-    const handleDeleteSelected = () => {
-        const newData = data.filter((_, index) => !checkedItems[index]);
-        setData(newData);
-        setCheckedItems(Array(newData.length).fill(false));
-    };
+  const handleDeleteSelected = () => {
+    const newData = data.filter((_, index) => !checkedItems[index]);
+    setData(newData);
+    setCheckedItems(Array(newData.length).fill(false));
+  };
 
-    useEffect(() => {
-        fetch('image.json')
-            .then(res => res.json())
-            .then(data => {
-                setData(data);
-                setCheckedItems(Array(data.length).fill(false));
-            });
-    }, []);
+  useEffect(() => {
+    fetch('image.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setCheckedItems(Array(data.length).fill(false));
+      });
+  }, []);
 
-    const checkedItemCount = checkedItems.filter(item => item).length;
+  const handleDragStart = (index) => {
+    setDraggedIndex(index);
+  };
 
-    return (
-        <div className='container mx-auto'>
-            <div className='flex justify-between text-lg'>
-                <h1>{checkedItemCount === 0 ? 'Gallery' : `Checked items: ${checkedItemCount}`}</h1>
-                {checkedItemCount > 0 && <button onClick={handleDeleteSelected}>Delete Items</button>}
+  const handleDragOver = (index) => {
+    if (index !== draggedIndex) {
+      const newData = [...data];
+      const [draggedItem] = newData.splice(draggedIndex, 1);
+      newData.splice(index, 0, draggedItem);
+      setData(newData);
+      setDraggedIndex(index);
+    }
+  };
+
+  const handleDragEnd = () => {
+    setDraggedIndex(null);
+  };
+
+  return (
+    <div className="container mx-auto divide-y">
+      <div className="flex justify-between text-lg my-4">
+        <h1>{checkedItems.every((item) => !item) ? 'Gallery' : `Checked items: ${checkedItems.filter(Boolean).length}`}</h1>
+        {checkedItems.some((item) => item) && <button onClick={handleDeleteSelected}>Delete Items</button>}
+      </div>
+      <div className="imageCal py-10">
+        {data.map((item, index) => (
+          <div
+            key={index}
+            className={`individual ${index === 0 ? 'large' : ''} ${index === draggedIndex ? 'dragged' : ''}`}
+            draggable
+            onDragStart={() => handleDragStart(index)}
+            onDragOver={() => handleDragOver(index)}
+            onDragEnd={handleDragEnd}
+          >
+            <img className="imageSize" src={item.image} alt="" />
+            <div className={checkedItems[index] ? 'checked' : 'layout'}>
+              <input
+                type="checkbox"
+                checked={checkedItems[index] || false}
+                onChange={() => handleCheckboxChange(index)}
+                className="checkbox checkboxdesign rounded-sm text-cyan-400 checkbox-error"
+              />
             </div>
-            <div className='imageCal'>
-                {data.map((item, index) => (
-                    <div key={index} className={`individual ${index === 0 ? 'large' : ''}`}>
-                        <img className='imageSize' src={item.image} alt="" />
-                        <div className={checkedItems[index] ? 'checked' : 'layout'}>
-                            <input
-                                type="checkbox"
-                                checked={checkedItems[index] || false}
-                                onChange={() => handleCheckboxChange(index)}
-                                className='checkbox checkboxdesign rounded-sm text-cyan-400 checkbox-error'
-                            />
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
